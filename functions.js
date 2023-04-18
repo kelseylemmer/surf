a70c2dcbb586cc7ac903b115c717f27944694a2c
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-04-18 15:20:07 -0400

    final book 1 project 3

---
+++ functions.js
@@ -0,0 +1,61 @@
+
+const isBargain = (inventoryItem) => {
+    let returnValue = false
+
+    if (inventoryItem.price < 50.00) {
+        returnValue = true
+    }
+
+    return returnValue
+}
+
+const isClothing = (item) => {
+    let returnValue = false
+
+    if (item.type === "clothing") {
+        returnValue = true
+    }
+
+    return returnValue
+}
+
+const isSurfboard = (item) => {
+    let returnValue = false
+
+    if (item.type === "surfboard") {
+        returnValue = true
+    }
+
+    return returnValue
+}
+
+const isGear = (gear) => {
+    let returnValue = false
+
+    if (gear.type === "gear") {
+        returnValue = true
+    }
+
+    return returnValue
+}
+
+const convertDataForAccounting = (product) => {
+    const allUpperCase = `${product.description.toUpperCase()} - ${product.type.toUpperCase()} - ${product.price}`
+    return allUpperCase
+}
+
+const calculateAveragePrice = (products) => {
+    let sumPrice = 0
+    for (const item of products) {
+        sumPrice += item.price
+    }
+    const averagePrice = sumPrice / products.length
+
+    return averagePrice
+}
+
+module.exports = {
+    isBargain, isClothing, isGear,
+    isSurfboard, convertDataForAccounting,
+    calculateAveragePrice
+}
